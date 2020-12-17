19d11ab8c83c6e4415b818b238ae025fd3f25317
Lawand Othman <lwnd@pm.me>
2020-12-17 13:09:30 +0000

    connect to DB

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,11 @@
 import express from 'express'
-import { config } from 'dotenv'
+import dotenv from 'dotenv'
+import connectDB from './config/db.js'
 import products from './data/products.js'
 
-config()
+dotenv.config()
+
+connectDB()
 
 const app = express()
 
