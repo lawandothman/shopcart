2cc9f8cc027c0050692342d3d634b82863e26e95
Lawand Othman <lwnd@pm.me>
2020-12-17 09:22:40 +0000

    Home Screen and Product listing

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,14 @@
 import { Container } from 'react-bootstrap'
 import Header from './components/Header'
 import Footer from './components/Footer'
+import HomeScreen from './screens/HomeScreen'
 
 const App = () => (
   <>
     <Header />
     <main className='py-3'>
       <Container>
-        <h1>Welcome to Shopcart</h1>
+        <HomeScreen />
       </Container>
     </main>
     <Footer />
---
+++ frontend/src/screens/HomeScreen.js
@@ -0,0 +1,18 @@
+import { Row, Col } from 'react-bootstrap'
+import products from '../products'
+import Product from '../components/Product'
+
+const HomeScreen = () => (
+  <>
+    <h1>Latest Products</h1>
+    <Row>
+      {products.map((product) => (
+        <Col sm={12} md={6} lg={4} xlg={3}>
+          <Product product={product} />
+        </Col>
+      ))}
+    </Row>
+  </>
+)
+
+export default HomeScreen
