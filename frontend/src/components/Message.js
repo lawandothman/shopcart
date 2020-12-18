9cd89d9caa4dfcdcc2cba810d9de17ae18813bd1
Lawand Othman <lwnd@pm.me>
2020-12-18 09:52:38 +0000

    add message and loader components

---
+++ frontend/src/components/Message.js
@@ -0,0 +1,13 @@
+import { Alert } from 'react-bootstrap'
+
+const Message = ({ variant, children }) => (
+  <Alert variant={variant}>
+    {children}
+  </Alert>
+)
+
+Message.defaultProps = {
+  variant: 'info',
+}
+
+export default Message
