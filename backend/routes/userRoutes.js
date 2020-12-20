245dc4f82988807b994231dd69ac0379dba902e3
Lawand Othman <lwnd@pm.me>
2020-12-20 21:19:26 +0000

    add user registration and password encryption

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,9 +1,10 @@
 import express from 'express'
-import { authUser, getUserProfile } from '../controllers/userController.js'
+import { authUser, registerUser, getUserProfile } from '../controllers/userController.js'
 import { protect } from '../middleware/authMiddleware.js'
 
 const router = express.Router()
 
+router.route('/').post(registerUser)
 router.route('/login').post(authUser)
 router.route('/profile').get(protect, getUserProfile)
 
