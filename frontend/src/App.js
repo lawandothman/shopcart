f2bfbfb325fcb1c07c987c9f70094cc5d72cad1f
Lawand Othman <lwnd@pm.me>
2020-12-21 11:35:14 +0000

    add profile screen and get user details

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -7,6 +7,7 @@ import ProdcuctScreen from './screens/ProductScreen'
 import CartScreen from './screens/CartScreen'
 import LoginScreen from './screens/LoginScreen'
 import RegisterScreen from './screens/RegisterScreen'
+import ProfileScreen from './screens/ProfileScreen'
 
 const App = () => (
   <Router>
@@ -15,6 +16,7 @@ const App = () => (
       <Container>
         <Route path='/login' component={LoginScreen} />
         <Route path='/register' component={RegisterScreen} />
+        <Route path='/profile' component={ProfileScreen} />
         <Route path='/product/:id' component={ProdcuctScreen} />
         <Route path='/cart/:id?' component={CartScreen} />
         <Route path='/' component={HomeScreen} exact />
---
+++ frontend/src/screens/ProfileScreen.js
@@ -0,0 +1,105 @@
+import { useState, useEffect } from 'react'
+import {
+  Form, Row, Col, Button,
+} from 'react-bootstrap'
+import { useDispatch, useSelector } from 'react-redux'
+import Message from '../components/Message'
+import Loader from '../components/Loader'
+import { getUserDetails } from '../actions/userActions'
+
+const ProfileScreen = ({ history }) => {
+  const [name, setName] = useState('')
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+  const [confirmPassword, setConfirmPassword] = useState('')
+  const [message, setMessage] = useState('')
+
+  const dispatch = useDispatch()
+
+  const userDetails = useSelector((state) => state.userDetails)
+  const { loading, error, user } = userDetails
+
+  const userLogin = useSelector((state) => state.userLogin)
+  const { userInfo } = userLogin
+
+  useEffect(() => {
+    if (!userInfo) {
+      history.push('/login')
+    } else if (!user.name) {
+      dispatch(getUserDetails('profile'))
+    } else {
+      setName(user.name)
+      setEmail(user.email)
+    }
+  }, [dispatch, history, userInfo, user])
+
+  const submitHandler = (e) => {
+    e.preventDefault()
+    if (password !== confirmPassword) {
+      setMessage('Passwords do not match')
+    } else {
+      // dispatch update profile
+    }
+  }
+
+  return (
+    <Row>
+      <Col md={3}>
+        <h2>User Profile</h2>
+        {message && <Message variant='danger'>{message}</Message>}
+        {error && <Message variant='danger'>{error}</Message>}
+        {loading && <Loader />}
+        <Form onSubmit={submitHandler}>
+          <Form.Group controlId='name'>
+            <Form.Label>Name</Form.Label>
+            <Form.Control
+              type='name'
+              placeholder='Enter name'
+              value={name}
+              onChange={(e) => setName(e.target.value)}
+            />
+          </Form.Group>
+
+          <Form.Group controlId='email'>
+            <Form.Label>Email Address</Form.Label>
+            <Form.Control
+              type='email'
+              placeholder='Enter email'
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            />
+          </Form.Group>
+
+          <Form.Group controlId='password'>
+            <Form.Label>Password</Form.Label>
+            <Form.Control
+              type='password'
+              placeholder='Enter password'
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            />
+          </Form.Group>
+
+          <Form.Group controlId='confirmPassword'>
+            <Form.Label>Confirm Password</Form.Label>
+            <Form.Control
+              type='password'
+              placeholder='Confirm password'
+              value={confirmPassword}
+              onChange={(e) => setConfirmPassword(e.target.value)}
+            />
+          </Form.Group>
+
+          <Button type='submit' variant='primary'>
+            Update
+          </Button>
+        </Form>
+      </Col>
+      <Col md={9}>
+        <h2>My orders</h2>
+      </Col>
+    </Row>
+  )
+}
+
+export default ProfileScreen
