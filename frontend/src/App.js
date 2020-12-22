84af039d5228ad9f4e7d2ca5c5b496e81a9fdefd
Lawand Othman <lwnd@pm.me>
2020-12-22 09:31:46 +0000

    add shipping screen & save address to localStorage

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -8,12 +8,14 @@ import CartScreen from './screens/CartScreen'
 import LoginScreen from './screens/LoginScreen'
 import RegisterScreen from './screens/RegisterScreen'
 import ProfileScreen from './screens/ProfileScreen'
+import ShippingScreen from './screens/ShippingScreen'
 
 const App = () => (
   <Router>
     <Header />
     <main className='py-3'>
       <Container>
+        <Route path='/shipping' component={ShippingScreen} />
         <Route path='/login' component={LoginScreen} />
         <Route path='/register' component={RegisterScreen} />
         <Route path='/profile' component={ProfileScreen} />
