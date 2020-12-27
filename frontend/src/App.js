4aafc1370761b73f387414fd12836e8e8251e6bf
Lawand Othman <lwnd@pm.me>
2020-12-27 19:48:08 +0000

    add user list for admin in FE

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -12,6 +12,7 @@ import ShippingScreen from './screens/ShippingScreen'
 import PaymentScreen from './screens/PaymentScreen'
 import PlaceOrderScreen from './screens/PlaceOrderScreen'
 import OrderScreen from './screens/OrderScreen'
+import UserListScreen from './screens/UserListScreen'
 
 const App = () => (
   <Router>
@@ -27,6 +28,7 @@ const App = () => (
         <Route path='/profile' component={ProfileScreen} />
         <Route path='/product/:id' component={ProdcuctScreen} />
         <Route path='/cart/:id?' component={CartScreen} />
+        <Route path='/admin/userlist' component={UserListScreen} />
         <Route path='/' component={HomeScreen} exact />
       </Container>
     </main>
