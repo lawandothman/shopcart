1d8934d6c8671185eca4b5b3271f42ffc5fbb14f
Lawand Othman <lwnd@pm.me>
2021-02-06 23:19:18 +0000

    get user by ID and update user admin routes

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -139,6 +139,48 @@ const deleteUser = asyncHandler(async (req, res) => {
   res.json(user)
 })
 
+/**
+ * @description     Get user by ID
+ * @route           GET /api/users/:id
+ * @access          Private/Admin
+ */
+const getUserById = asyncHandler(async (req, res) => {
+  const user = await User.findById(req.params.id).select('-password')
+  if (user) {
+    res.json(user)
+  } else {
+    res.status(404)
+    throw new Error('User not found')
+  }
+})
+
+/**
+ * @description     Update user
+ * @route           PUT /api/users/:id
+ * @access          Private/Admin
+ */
+const updateUser = asyncHandler(async (req, res) => {
+  const user = await User.findById(req.params.id)
+
+  if (user) {
+    user.name = req.body.name || user.name
+    user.email = req.body.email || user.email
+    user.isAdmin = req.body.isAdmin
+
+    const updatedUser = await user.save()
+
+    res.json({
+      _id: updatedUser._id,
+      name: updatedUser.name,
+      email: updatedUser.email,
+      isAdmin: updatedUser.isAdmin,
+    })
+  } else {
+    res.status(404)
+    throw new Error('User not found')
+  }
+})
+
 export {
   authUser,
   registerUser,
@@ -146,4 +188,6 @@ export {
   updateUserProfile,
   getUsers,
   deleteUser,
+  getUserById,
+  updateUser,
 }
