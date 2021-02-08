ec8d983e586bfa5b678f9a0ba0dd7344a9761d3a
Lawand Othman <lwnd@pm.me>
2021-02-08 11:59:59 +0000

    update user functionality - admin

--- frontend/src/reducers/userReducers.js
+++ frontend/src/reducers/userReducers.js
@@ -20,6 +20,10 @@ import {
   USER_LIST_RESET,
   USER_DELETE_REQUEST,
   USER_DELETE_SUCCESS,
+  USER_UPDATE_REQUEST,
+  USER_UPDATE_SUCCESS,
+  USER_UPDATE_FAIL,
+  USER_UPDATE_RESET,
 } from '../constants/userConstants'
 
 export const userLoginReducer = (state = {}, action) => {
@@ -65,7 +69,7 @@ export const userDetailsReducer = (state = { user: {} }, action) => {
   }
 }
 
-export const userUpdateProfileReducer = (state = { }, action) => {
+export const userUpdateProfileReducer = (state = {}, action) => {
   switch (action.type) {
     case USER_UPDATE_PROFILE_REQUEST:
       return { ...state, loading: true }
@@ -95,7 +99,7 @@ export const userListReducer = (state = { users: [] }, action) => {
   }
 }
 
-export const userDeleteReducer = (state = { }, action) => {
+export const userDeleteReducer = (state = {}, action) => {
   switch (action.type) {
     case USER_DELETE_REQUEST:
       return { ...state, loading: true }
@@ -103,6 +107,21 @@ export const userDeleteReducer = (state = { }, action) => {
       return { loading: false, success: true }
     case USER_DETAILS_FAIL:
       return { loading: false, error: action.payload }
+    default:
+      return state
+  }
+}
+
+export const userUpdateReducer = (state = { user: {} }, action) => {
+  switch (action.type) {
+    case USER_UPDATE_REQUEST:
+      return { loading: true }
+    case USER_UPDATE_SUCCESS:
+      return { loading: false, success: true }
+    case USER_UPDATE_FAIL:
+      return { loading: false, error: action.payload }
+    case USER_UPDATE_RESET:
+      return { user: {} }
     default:
       return state
   }
--- frontend/src/screens/UserEditScreen.js
+++ frontend/src/screens/UserEditScreen.js
@@ -5,7 +5,8 @@ import { useDispatch, useSelector } from 'react-redux'
 import Message from '../components/Message'
 import Loader from '../components/Loader'
 import FormContainer from '../components/FormContainer'
-import { getUserDetails } from '../actions/userActions'
+import { getUserDetails, updateUser } from '../actions/userActions'
+import { USER_UPDATE_RESET } from '../constants/userConstants'
 
 const userEditScreen = ({ match, history }) => {
   const userId = match.params.id
@@ -18,19 +19,36 @@ const userEditScreen = ({ match, history }) => {
 
   const userDetails = useSelector((state) => state.userDetails)
   const { loading, error, user } = userDetails
+  const userUpdate = useSelector((state) => state.userUpdate)
+  const {
+    loading: loadingUpdate,
+    error: errorUpdate,
+    success: successUpdate,
+  } = userUpdate
 
   useEffect(() => {
-    if (!user.name || user._id !== userId) {
+    if (successUpdate) {
+      dispatch({ type: USER_UPDATE_RESET })
+      history.push('/admin/userlist')
+    } else if (!user.name || user._id !== userId) {
       dispatch(getUserDetails(userId))
     } else {
       setName(user.name)
       setEmail(user.email)
       setIsAdmin(user.isAdmin)
     }
-  }, [dispatch, user, userId])
+  }, [dispatch, user, userId, successUpdate, history])
 
   const submitHandler = (e) => {
     e.preventDefault()
+    dispatch(
+      updateUser({
+        _id: userId,
+        name,
+        email,
+        isAdmin,
+      }),
+    )
   }
 
   return (
@@ -40,6 +58,8 @@ const userEditScreen = ({ match, history }) => {
       </Link>
       <FormContainer>
         <h1>Edit User</h1>
+        {loadingUpdate && <Loader /> }
+        {errorUpdate && <Message variant='danger'>{errorUpdate}</Message> }
         {loading ? <Loader /> : error ? <Message variant='danger'>{error}</Message>
           : (
             <Form onSubmit={submitHandler}>
