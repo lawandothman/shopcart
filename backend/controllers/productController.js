7a7b4ab4a1566409eb2e0758ae9d76182c6ed899
Lawand Othman <lwnd@pm.me>
2021-02-10 11:42:03 +0000

    delete product end point

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -28,4 +28,21 @@ const getProductById = asyncHandler(async (req, res) => {
   }
 })
 
-export { getProducts, getProductById }
+/**
+ * @description     Delete a product
+ * @route           DELETE /api/products/:id
+ * @access          Private/Admin
+ */
+const deleteProduct = asyncHandler(async (req, res) => {
+  const product = await Product.findById(req.params.id)
+
+  if (product) {
+    await product.remove()
+    res.json({ message: 'Product removed' })
+  } else {
+    res.status(404)
+    throw new Error('Product not found')
+  }
+})
+
+export { getProducts, getProductById, deleteProduct }
