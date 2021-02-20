a56c41c73247bb38ec5229df42dcce2fa800248a
Lawand Othman <lwnd@pm.me>
2021-02-20 17:47:02 +0000

    create product review endpoint

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -5,6 +5,7 @@ import {
   deleteProduct,
   updateProduct,
   createProduct,
+  createProductReview,
 } from '../controllers/productController.js'
 import { protect, admin } from '../middleware/authMiddleware.js'
 
@@ -16,5 +17,6 @@ router
   .get(getProductById)
   .delete(protect, admin, deleteProduct)
   .put(protect, admin, updateProduct)
+router.route('/:id/reviews').post(protect, createProductReview)
 
 export default router
