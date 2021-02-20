d17edae87bc08bfc8bc37c312bcdd05da8d6590f
Lawand Othman <lwnd@pm.me>
2021-02-20 17:31:51 +0000

    install morgan

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 import path from 'path'
 import express from 'express'
 import dotenv from 'dotenv'
+import morgan from 'morgan'
 import connectDB from './config/db.js'
 import productRoutes from './routes/productRoutes.js'
 import userRoutes from './routes/userRoutes.js'
@@ -15,6 +16,10 @@ connectDB()
 const app = express()
 
 app.use(express.json())
+
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'))
+}
 
 app.get('/', (req, res) => {
   res.send('API is running ...')
