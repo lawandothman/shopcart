add6b43950ab615f219867df970d70ecd223fa64
Lawand Othman <lwnd@pm.me>
2021-02-20 16:43:33 +0000

    order routes

--- backend/routes/orderRoutes.js
+++ backend/routes/orderRoutes.js
@@ -3,7 +3,7 @@ import {
   addOrderItems,
   getOrderById,
   updateOrderToPaid,
-  updateOrderToDelivered,
+  updateOrderToDispatched,
   getMyOrders,
   getOrders,
 } from '../controllers/orderController.js'
@@ -15,6 +15,6 @@ router.route('/').post(protect, addOrderItems).get(protect, admin, getOrders)
 router.route('/myorders').get(protect, getMyOrders)
 router.route('/:id').get(protect, getOrderById)
 router.route('/:id/pay').put(protect, updateOrderToPaid)
-router.route('/:id/deliver').put(protect, admin, updateOrderToDelivered)
+router.route('/:id/deliver').put(protect, admin, updateOrderToDispatched)
 
 export default router
