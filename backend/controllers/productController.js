7df223f2cd22a960a042e461f01cbd0588bd1697
Lawand Othman <lwnd@pm.me>
2021-02-26 09:06:08 +0000

    products pagination

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -154,6 +154,17 @@ const createProductReview = asyncHandler(async (req, res) => {
   }
 })
 
+/**
+ * @description     Get top rated products
+ * @route           GET /api/products/top
+ * @access          Public
+ */
+const getTopProducts = asyncHandler(async (req, res) => {
+  const products = await Product.find({}).sort({ rating: -1 }).limit(3)
+
+  res.json(products)
+})
+
 export {
   getProducts,
   getProductById,
@@ -161,4 +172,5 @@ export {
   createProduct,
   updateProduct,
   createProductReview,
+  getTopProducts,
 }
--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -5,6 +5,7 @@ import Product from '../components/Product'
 import Message from '../components/Message'
 import Loader from '../components/Loader'
 import Paginate from '../components/Paginate'
+import ProductCarousel from '../components/ProductCarousel'
 import { listProducts } from '../actions/productActions'
 
 const HomeScreen = ({ match }) => {
@@ -23,6 +24,7 @@ const HomeScreen = ({ match }) => {
 
   return (
     <>
+      {!keyword && <ProductCarousel />}
       <h1>Latest Products</h1>
       {loading ? (
         <Loader />
