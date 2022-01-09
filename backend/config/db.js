2095fd3fccce77943faf212322b7d81eb735d144
Lawand Othman <lwnd@pm.me>
2022-01-09 23:15:49 +0000

    Fix

--- backend/config/db.js
+++ backend/config/db.js
@@ -5,7 +5,7 @@ const connectDB = async () => {
     const conn = await mongoose.connect(process.env.MONGO_URI, {
       useUnifiedTopology: true,
       useNewUrlParser: true,
-      useCreateIndex: true,
+      // useCreateIndex: true,
     })
     console.log(`✨ MongoDB Connected: ${conn.connection.host}`)
   } catch (error) {
